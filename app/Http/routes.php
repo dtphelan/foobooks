e50f84ab7fea19c302b61c422d77791f26bd41e7
dtphelan <dtphelan@gmail.com>
2016-03-04 15:25:54 -0500

    Env practice

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,8 +4,17 @@ Route::group(['middleware' => ['web']], function () {
         #return view('welcome');
         return 'Hello welcome to my Laravel application!';
     });
+
     Route::get('/books', 'BookController@getIndex');
     Route::get('/book/create', 'BookController@getCreate');
     Route::post('/book/create', 'BookController@postCreate');
     Route::get('/book/{id}', 'BookController@getShow');
+
+    Route::get('/practice', function() {
+
+        echo config('app.url');
+        return '';
+
+    });
+
 });
